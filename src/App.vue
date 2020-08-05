2ebd2b1edd6da5c48a8c8b108a5decae91d2d612
lukelong <lukelongdad@gmail.com>
2020-08-05 10:18:56 +0800

    add file

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="header-part">顶部</div>
+        <div class="content-box">
+            <div class="nav">
+                <p>
+                    <router-link to='/home'>首页</router-link>
+                </p>
+                <p>
+                    <router-link to='/article'>文章</router-link>
+                </p>
+            </div>
+            <div class="content">
+                <router-view></router-view>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    el: 'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.header-part {
+    background: red;
+}
+.nav {
+    background: antiquewhite;
+    height: 500px;
+    width: 159px;
+    float: left;
+}
+.content {
+    background: aliceblue;
+    height: 500px;
+    width: 800px;
+    float: left;
 }
 </style>
